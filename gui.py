55866fb925fa3d9b89ccfd35056d5ca346cfbfaa
sk-samir <samir.sk26318@gmail.com>
2024-09-24 11:05:02 -0500

    Implement Complete and Exit Button

--- gui.py
+++ gui.py
@@ -7,17 +7,22 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
                    layout=[[label],
                            [input_box, add_button],
-                           [list_box, edit_button]],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])  # List
+    print(4, values['todos'][0])  # String
     match event:
         case "Add":
             todos = get_todos()
@@ -28,19 +33,31 @@ while True:
             window['todos'].update(values=todos)
 
         case "Edit":
-            todo_to_dit = values['todos'][0]
+            todo_to_edit = values['todos'][0]   # This is for getting string from list
             new_todo = values['todo']
 
             todos = get_todos()
-            index = todos.index(todo_to_dit)
+            index = todos.index(todo_to_edit)
             todos[index] = new_todo
             write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = get_todos()
+            todos.remove(todo_to_complete)
+            write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+
+        case "Exit":
+            break
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
         case sg.WINDOW_CLOSED:
-            break
+            break  # exit current loop
+            # exit() # exit the program
 
 window.close()
